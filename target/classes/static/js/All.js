b3eb6bc43a51408da8cc77182d1f3092ce05b832
Archerxzs <xzs0116@163.com>
2021-06-09 13:08:43 +0800

    fix bug

--- target/classes/static/js/All.js
+++
@@ -1,8 +0,0 @@
-// $(document).ready(function () {
-//     $("#search").on("keypress",function (event) {
-//         var inputValue=$("#search").val()
-//         if (event.keyCode === 13) {
-//             location.href="/ida/scholar/findscholarsbynamelike?scholarName="+inputValue;
-//         }
-//     })
-// })
--- target/classes/static/js/Header.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    console.log("Header.js")
-    $("#search").on("keypress",function (event) {
-        var inputValue=$("#search").val()
-        if (event.keyCode === 13) {
-            location.href="/ida/all/findallbyanywords?words="+inputValue
-        }
-    })
-})
